70bf2c1c4c42aa06454a71fdbe34313d15c8e430
Steven <stevendiaz512@gmail.com>
2024-02-15 23:49:19 -0500

    updates

--- Javascript/check_admin_login.js
+++ Javascript/check_admin_login.js
@@ -1,6 +1,6 @@
 import { supabase } from "/Javascript/database.js";
 
-
+async function getAdminData(){
     const email = document.getElementById("inputEmail").value;
     const pw = document.getElementById("inputPassword").value;
 
@@ -8,14 +8,14 @@ import { supabase } from "/Javascript/database.js";
     console.log(pw)
 
     const { data, error } = await supabase
-            .from("Admin_table")
-            .select(email_address, password);
-            console.log( data)
+    .from("Admin_table")
+    .select(email_address, password);
+    console.log( data)
     
           if (email_address == email && password == pw){
             console.log("Success 101")
-            //window.location.href = 'HTML\admin\main_admin_page.html'
+            window.location.href = 'HTML\admin\main_admin_page.html'
           }  
-
-      
-
+          else 
+            throw error
+}
