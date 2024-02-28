54de5935ff0c0ab086767b458ac44706cb8adf78
Steven <stevendiaz512@gmail.com>
2024-02-28 12:23:03 -0500

    added show pass feature

---
+++ Javascript/admin_function.js
@@ -0,0 +1,8 @@
+function showPass() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
